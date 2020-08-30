cb8681a23385486ef5d106a8bb89478c896ece12
Francisco Possetto <franciscopossetto@hotmail.com>
2020-08-30 16:58:30 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# patternlang
+
+> A pattern language for Architecuture
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+## Notes
+
+# install bulma
+npm i -S bulma
+
+# install pug
+npm i -D pug pug-loader 
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+#app
+	.container
+		h1 {{ title }}
+		h2 {{ subtitle }}
+		h3 {{ text }}
+
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'patternlang.com',
+      subtitle: 'Based on the book "A pattern language for Architecture", by Christopher Alexander',
+      text: 'this project add computacional complexity to the original schema, showing patterns that could be programmed with Softwares like Revit or Rhino'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/main.scss'
+
+</style>
